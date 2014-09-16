8e2759f8532da3d68b755901eff2c5bca1e58c9c
utz-michael <michael.utz@ndsatcom.com>
2014-09-16 22:03:48 +0200

    delays angepasst

--- DigitalPreassureGauge.ino
+++ DigitalPreassureGauge.ino
@@ -21,17 +21,18 @@ void setup() {
     lcd.print("    Digital     ");
     lcd.setCursor(0, 1);
     lcd.print(" Pressure Gauge ");
-delay(2000);
+delay(3000);
 
    
     lcd.setCursor(0, 0);
     lcd.print("Calibrate Sensor");
     lcd.setCursor(0, 1);
     lcd.print("Fuelpump off    ");
+delay(3000);    
     
 for(int x =0 ; x < 1000 ; x++){
  
- FuelMainCal = FuelMainCal + analogRead(0); 
+ FuelMainCal = FuelMainCal + analogRead(1); 
  FuelCarburtorCal = FuelCarburtorCal + analogRead(2); 
  FuelNOSCal = FuelNOSCal + analogRead(3); 
 
@@ -48,26 +49,26 @@ for(int x =0 ; x < 1000 ; x++){
   Serial.print("FuelNOS_cal: ");
   Serial.println(FuelNOSCal);
  
- 
+  
     lcd.setCursor(0, 0);
     lcd.print("   Switch on    ");
     lcd.setCursor(0, 1);
     lcd.print("    Fuelpump    ");
-    
+ /*   
  do
 {
   delay(50);          // wait for sensors to stabilize
- FuelMain = analogRead(0);  // check the sensors
+ FuelMain = analogRead(1);  // check the sensors
 
 } while (FuelMain < FuelMainCal +10 );   
 
-
-
+*/
+delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-FuelMain = analogRead(0);
+FuelMain = analogRead(1);
 FuelCarburtor = analogRead(2);
 FuelNOS = analogRead(3);
 
